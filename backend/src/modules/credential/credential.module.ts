5e38c30b769597e8ecf723b4152ae196312fc8a1
andriii <andri@honet.web.id>
2026-05-25 12:57:12 +0700

    fix: remove forwardRef from credential module

--- backend/src/modules/credential/credential.module.ts
+++ backend/src/modules/credential/credential.module.ts
@@ -1,11 +1,11 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CredentialService } from './credential.service';
 import { CredentialController } from './credential.controller';
 import { EncryptionService } from '../../common/utils/encryption.service';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
+  imports: [AuthModule],
   controllers: [CredentialController],
   providers: [CredentialService, EncryptionService],
   exports: [CredentialService, EncryptionService],
